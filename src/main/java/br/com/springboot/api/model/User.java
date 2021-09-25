29e17a479a0edf00ea1d7213a324ceb75e99accf
Délio Marcos <deliomarcos.contato@gmail.com>
2021-09-25 20:08:21 -0300

    first commit

---
+++ src/main/java/br/com/springboot/api/model/User.java
@@ -0,0 +1,35 @@
+package br.com.springboot.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	private Long id;
+	private String name;
+	private String username;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
